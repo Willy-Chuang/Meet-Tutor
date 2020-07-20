348f23f74442792ba0cd29475b0fa10c8088b92d
Willy-Chuang <ws18950914@gmail.com>
2020-07-20 21:28:22 +0800

    home fragment layout

--- app/src/main/java/com/willy/metu/home/HomeFragment.kt
+++ app/src/main/java/com/willy/metu/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.willy.metu.databinding.FragmentHomeBinding
 import com.willy.metu.ext.getVmFactory
 
@@ -19,6 +21,16 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val recommendAdapter = RecommendAdapter()
+
+        binding.viewModel = viewModel
+        binding.recyclerRecommendation.adapter = recommendAdapter
+        binding.recyclerRecommendation.layoutManager = LinearLayoutManager(context)
+
+
+        viewModel.recommendUsers.observe(viewLifecycleOwner, Observer {
+            recommendAdapter.submitList(it)
+        })
 
         return binding.root
     }
