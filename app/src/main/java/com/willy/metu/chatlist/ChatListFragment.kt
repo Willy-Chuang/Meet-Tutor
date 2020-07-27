3f7b7e037bfc1f20ee89f5c046092eadc897d70d
Willy-Chuang <ws18950914@gmail.com>
2020-07-27 21:58:13 +0800

    add no value image to message

--- app/src/main/java/com/willy/metu/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/willy/metu/chatlist/ChatListFragment.kt
@@ -8,7 +8,6 @@ import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.willy.metu.R
 import com.willy.metu.data.ChatRoom
 import com.willy.metu.databinding.FragmentChatListBinding
@@ -55,7 +54,13 @@ class ChatListFragment : Fragment() {
             it?.let {
 
                 binding.recyclerChatList.layoutAnimation = AnimationUtils.loadLayoutAnimation(context, R.anim.recycler_animation)
-                adapter.submitList(it)
+
+                if (it.isEmpty()) {
+                    binding.noValue.visibility = View.VISIBLE
+                    binding.noValueImage.visibility = View.VISIBLE
+                } else {
+                    adapter.submitList(it)
+                }
             }
         })
 
