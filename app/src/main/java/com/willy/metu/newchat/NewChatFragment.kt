77ccbdb63b0ef420ed4d89bff58e2230662612d5
Willy-Chuang <ws18950914@gmail.com>
2020-08-07 11:50:29 +0800

    refactor new chat

--- app/src/main/java/com/willy/metu/newchat/NewChatFragment.kt
+++ app/src/main/java/com/willy/metu/newchat/NewChatFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.willy.metu.NavigationDirections
 import com.willy.metu.databinding.FragmentNewChatBinding
 import com.willy.metu.ext.getVmFactory
@@ -18,60 +17,64 @@ class NewChatFragment : Fragment() {
 
     private val viewModel by viewModels<NewChatViewModel> { getVmFactory() }
 
+    lateinit var binding: FragmentNewChatBinding
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentNewChatBinding.inflate(inflater, container, false)
+        binding = FragmentNewChatBinding.inflate(inflater, container, false)
         val adapter = NewChatAdapter()
 
         binding.viewModel = viewModel
-        binding.recyclerFollowList.layoutManager = LinearLayoutManager(context)
         binding.recyclerFollowList.adapter = adapter
-
-
 
 
         viewModel.allFollowedList.observe(viewLifecycleOwner, Observer { all ->
             all?.let { users ->
 
                 if (users.isEmpty()) {
-                    binding.noValue.visibility = View.VISIBLE
-                    binding.noValueButton.visibility = View.VISIBLE
-                    binding.noValueButton.setOnClickListener {
-                        findNavController().navigate(NavigationDirections.navigateToPairingFragment())
-                    }
+                    chatListValueVisibility(false)
                 } else {
-                    binding.noValue.visibility =View.GONE
-                    binding.noValue.visibility = View.GONE
+                    chatListValueVisibility(true)
                 }
 
                 adapter.submitList(users)
 
-                viewModel.searchBy.observe(viewLifecycleOwner, Observer {
-
-                    val sortedList = users.sortByName(it)
-
-                    if (sortedList.isEmpty()) {
-                        binding.noValue.visibility = View.VISIBLE
-                        binding.noValueButton.visibility = View.VISIBLE
-                        binding.noValueButton.setOnClickListener {
-                            findNavController().navigate(NavigationDirections.navigateToPairingFragment())
-                        }
-                    } else {
-                        binding.noValue.visibility =View.GONE
-                        binding.noValue.visibility = View.GONE
-                    }
-
-                    adapter.submitList(sortedList)
-
-                })
             }
+        })
+
+        // Filter list based on the input of edit text
+        viewModel.searchBy.observe(viewLifecycleOwner, Observer {
+
+            val sortedList = viewModel.allFollowedList.value.sortByName(it)
+
+            if (sortedList.isEmpty()) {
+                chatListValueVisibility(false)
+            } else {
+                chatListValueVisibility(true)
+            }
+
+            adapter.submitList(sortedList)
+
         })
 
         return binding.root
 
+    }
+
+    private fun chatListValueVisibility(withValue: Boolean) {
+        if (withValue) {
+            binding.noValue.visibility = View.GONE
+            binding.noValueButton.visibility = View.GONE
+        } else {
+            binding.noValue.visibility = View.VISIBLE
+            binding.noValueButton.visibility = View.VISIBLE
+            binding.noValueButton.setOnClickListener {
+                findNavController().navigate(NavigationDirections.navigateToPairingFragment())
+            }
+        }
     }
 }
